4eab9d8ac0145c80dcc0df69f1ab7111a5850633
1c0d3rZ <adzohonouapelete@gmail.com>
2022-09-13 20:16:51 +0000

    server done

---
+++ server/config/database.js
@@ -0,0 +1,8 @@
+//Set up mongoose connection
+const mongoose = require('mongoose');
+// require('dotenv').config({ path: "./.env" });
+// const mongoDB = process.env.ATLAS_URI;
+const mongoDB = 'mongodb+srv://coderz:maak@cluster0.hobon.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+module.exports = mongoose;
